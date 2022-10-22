42ae2e3827b033fbba4c5cbcd095d64e00b2b5b4
GameFORAdventure <tolgaboraturksoy@gmail.com>
2022-10-22 04:56:47 +0300

    Revert "Merge branch 'main' of https://github.com/melihacil/GreenyGameJam2022"
    This reverts commit 49fcd05fa1e94bed8758b8f12851636149f338fb, reversing
    changes made to 8f2cf374c72abb157dbb8294bf75fcfeaf3fa06d.

---
+++ GreenyJamProject/Assets/Anıl/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    
+    void Update()
+    {
+        movement.x =Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed* Time.fixedDeltaTime);
+    }
+}
